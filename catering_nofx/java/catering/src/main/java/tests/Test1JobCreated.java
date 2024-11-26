3a7c08a145eeee888fd214e286e79a82835813e4
Marco Schiavone <marko.skiaa40@gmail.com>
2024-11-26 10:43:46 +0100

    minor changes to print format
    and comments rearranging

--- catering_nofx/java/catering/src/main/java/tests/Test1JobCreated.java
+++ catering_nofx/java/catering/src/main/java/tests/Test1JobCreated.java
@@ -14,40 +14,42 @@ public class Test1JobCreated {
             System.out.println("TEST Fake login");
             CatERing.getInstance().getUserManager().fakeLogin("Lidia");     // chef
             System.out.println(CatERing.getInstance().getUserManager().getCurrentUser());
-            System.out.println("\n---------------------\n");
+            System.out.println("---------------------\n");
 
-            Menu m = CatERing.getInstance().getMenuManager().getAllMenus().get(2);
+            Menu m = CatERing.getInstance().getMenuManager().getAllMenus().get(1);
             Service srv = new Service(m);
             srv.setId(rd.nextInt(100) + 1);
             srv.updateChef(CatERing.getInstance().getUserManager().getCurrentUser());
 
-            // 1st step
+            // step 1
             CatERing.getInstance().getJobManager().createJobsSheet(srv);
             JobsSheet js =  CatERing.getInstance().getJobManager().getCurrentSheet();
 
             System.out.println("TEST Service: " + js);
-            System.out.println("\n---------------------\n");
+            System.out.println("---------------------\n");
 
-            // 2nd step
+            // step 2
             CatERing.getInstance().getJobManager().insertJob(Task.loadRecipeById(10)); // pane al cioccolato
             System.out.println("TEST InsertJob: " + js);
-            System.out.println("\n---------------------\n");
+            System.out.println("---------------------\n");
 
-            // 2nd step (2)
+            // step 2 (2)
             CatERing.getInstance().getJobManager().insertJob(Task.loadRecipeById(20));
             System.out.println("TEST InsertJob (2): " + js);
-            System.out.println("\n---------------------\n");
+            System.out.println("---------------------\n");
 
             // ArrayList<Job> jobs = CatERing.getInstance().getJobManager().getCurrentSheet().getAllJobs();
             // CatERing.getInstance().getJobManager().modifyJob(jobs.get(3), 15, 20);
             // CatERing.getInstance().getJobManager().modifyJob(jobs.get(2), 18, 25);
 
-            // 3rd step
+            // step 3
             CatERing.getInstance().getJobManager().sortJobsSheet(Comparator.comparing(j -> j.getTask().getId()));
             System.out.println("TEST sortJobsSheet: " + js);
-            System.out.println("OK.");
+            System.out.println("---------------------\nOK.");
         } catch (UseCaseLogicException e) {
             System.err.println("Logic error in use case: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
         }
     }
 }
